f00f7d5463e1ec68d5e596c439c66c4d98c1fc99
clicker5@inbox.lv <clicker5@inbox.lv>
2020-08-08 11:47:25 +0300

    Add project files.

---
+++ Desk.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cards
+{
+    public partial class Desk : Form
+    {
+        public Desk()
+        {
+            InitializeComponent();
+            InitializeDesk();
+        }
+
+        private void InitializeDesk()
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void LoadCards_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
